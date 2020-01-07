7cfdf937ecb79e0dd167049b5d117eca0d72e74d
Paul <paul.roberts@c02xt4ncjgh5.amer.reisystems.com>
2020-01-07 10:49:02 -0500

    dropdown from the second click

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostListener, HostBinding} from '@angular/core';
+import {Directive, HostListener, HostBinding, ElementRef} from '@angular/core';
 
 @Directive({
     selector:'[appDropdown]'
@@ -7,7 +7,15 @@ import {Directive, HostListener, HostBinding} from '@angular/core';
 export class DropdownDirective{
     @HostBinding('class.open') isOpen = false;
     
-    @HostListener('click') toggleOpen(){
-        this.isOpen = !this.isOpen;
-    }
+    // @HostListener('click') toggleOpen(){
+    //     this.isOpen = !this.isOpen;
+    // }
+    /**
+     * closes the dropdown from the second click anywhere on the page
+     * @param event 
+     */
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+      }
+      constructor(private elRef: ElementRef) {}
 }
