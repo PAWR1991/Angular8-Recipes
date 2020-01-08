90f4f887db3f2ec5f6b3a01f1d57b790d6acf56b
Paul <paul.roberts@C02XT4NCJGH5.local>
2020-01-08 09:51:06 -0500

    Added ingredient to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,24 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 
         'This is simply Test', 
-        'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+        'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20),
+        ]
         ),
         new Recipe('An Another Recipe', 
         'This is simply Test', 
-        'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+        'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Cheese', 1),
+        ]
         )
       ];
       recipeSelected = new EventEmitter<Recipe>();
