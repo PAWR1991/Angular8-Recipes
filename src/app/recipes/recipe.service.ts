ea2b582a63a30b385c70c6fc2e7a5134fd8d4460
Paul <paul.roberts@C02XT4NCJGH5.local>
2020-01-08 10:04:08 -0500

    pass ingredients to shopping list by services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 
@@ -23,11 +25,19 @@ export class RecipeService {
       ];
       recipeSelected = new EventEmitter<Recipe>();
 
+      constructor(private slService: ShoppingListService){
+
+      }
+
       /**
        * slice makes a new list than returning the pointer 
        */
       getRecipes(){
           return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
       }
 
 
